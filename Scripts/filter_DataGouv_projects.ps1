f4aa4b6eec94ec1f768159b90b7b5d8b11e0c067
Luc Bougé <luc.bouge@ens-rennes.fr>
2021-04-17 21:21:45 +0200

    Work

--- Scripts/filter_DataGouv_projects.ps1
+++ Scripts/filter_DataGouv_projects.ps1
@@ -1,6 +1,19 @@
-$filtering_column = "Projet.Code_Decision_ANR"
-$filtering_expression = "^ANR-21" 
+# Retain CE projects only (-Match)
+$filtering_column1 = "Projet.Code_Decision_ANR"
+$filtering_expression1 = "^ANR-\d{2}-CE" 
 
+# Retain only those with "artificial" and then "intelligence" the English abstract (-Match),
+# not too far away, though
+$filtering_column2 = "Projet.Resume.anglais"
+$filtering_expression2 = "artificial.{1,20}intelligence" 
+
+# Exclude those projects from CE23 (-NotMatch)
+$filtering_column3 = "Projet.Code_Decision_ANR"
+$filtering_expression3 = "-CE23-"
+
+
+# You may wish to expand the list as willing! :-)  
+# Make sure to add corresponding Where-Object commands below, with the right filtering option
 
 #####################################################
 # Phase 1: Download the DataGouv data into a local file
@@ -44,12 +57,22 @@ else {
 Write-Host("Loading Excel file ${path}")
 
 $excel = Import-Excel -Path "$path"
-
-# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/where-object
-
-$result = $excel | Where-Object -Property "$filtering_column" -Match "$filtering_expression"
+$result = $excel 
 
 #####################################################
-# Phase 4: Export the result as an Excel file and show it up
+# Phase 4: Filter the result, using a sequence of positive (-Match) and negative (-NotMatch) patterns 
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/where-object
+
+# Positive filtering (-Match)
+$result = $result | Where-Object -Property "$filtering_column1" -Match "$filtering_expression1"
+
+# Positive filtering (-Match)
+$result = $result | Where-Object -Property "$filtering_column2" -Match "$filtering_expression2"
+
+# Negative filtering (-NotMatch)
+$result = $result | Where-Object -Property "$filtering_column3" -NotMatch "$filtering_expression3"
+
+#####################################################
+# Phase 5: Export the result as an Excel file and show it up
 
 $result | Export-Excel -Show -AutoSize -AutoFilter -FreezeTopRow
