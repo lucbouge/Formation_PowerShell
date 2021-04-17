063cad167ed8dee4d5275ca22d619240df42aec0
Luc Bougé <luc.bouge@ens-rennes.fr>
2021-04-17 21:22:08 +0200

    Work

---
+++ Scripts/filter_DataGouv_projects_with_function.ps1
@@ -0,0 +1,23 @@
+function test {
+    Param($line)
+    $test1 = $line."Projet.Code_Decision_ANR" -match "^ANR-20" 
+    $test2 = $line."Projet.Resume.anglais" -match "artificial.{1,20}intelligence" 
+    $test3 = $line."Projet.Code_Decision_ANR" -notmatch "-CE23-" 
+    return ($test1 -and $test2 -and $test3)
+}
+
+$data = "data.xlsx"
+$path = "${HOME}/${data}"
+
+$excel = Import-Excel -Path "$path"
+
+#####################################################
+   
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/where-object
+
+$result = $excel | Where-Object { test($_) } 
+
+
+#####################################################
+
+$result | Export-Excel -Show -AutoSize -AutoFilter -FreezeTopRow
