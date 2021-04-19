85d3ef4cad956aa50094c4e721e47921a9a02d50
Luc Bougé <luc.bouge@ens-rennes.fr>
2021-04-19 14:31:15 +0200

    Work

--- Scripts/filter_DataGouv_projects_with_function.ps1
+++ Scripts/filter_DataGouv_projects_with_function.ps1
@@ -1,5 +1,5 @@
-function test {
-    Param($line)
+function test($line) {
+    # Param($line)
     $test1 = $line."Projet.Code_Decision_ANR" -match "^ANR-20" 
     $test2 = $line."Projet.Resume.anglais" -match "artificial.{1,20}intelligence" 
     $test3 = $line."Projet.Code_Decision_ANR" -notmatch "-CE23-" 
